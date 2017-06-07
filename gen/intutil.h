2f2b3f3167f2f0f5c8be10a143f8101577b7008a
Ignatius <ice.emissary@gmail.com>
2017-06-07 17:26:39 +0800

    修改 nextmul 模組名稱及內部函式名稱，以符合命名慣例。
    將模組名稱更名為 intutil (integer utility)，並為所有函式加上模組名前綴。
    將 next_* 函式更名為 ceil_*，以符合 C 語言普遍的命名慣例。

--- gen/intutil.h
+++ gen/intutil.h
@@ -1,14 +1,14 @@
 /**
  * @file
- * @brief     Multiple value calculator.
- * @details   To calculate the next value of a specific multiple of value.
+ * @brief     Integer utility.
+ * @details   Some small and simple utility of integer calculation.
  * @author    王文佑
  * @date      2016.03.19
  * @copyright ZLib Licence
  * @see       http://www.openfoundry.org/of/projects/2419
  */
-#ifndef _GEN_NEXTMUL_H_
-#define _GEN_NEXTMUL_H_
+#ifndef _GEN_INTUTIL_H_
+#define _GEN_INTUTIL_H_
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -19,24 +19,24 @@ extern "C" {
 #endif
 
 static inline
-bool is_pow2(unsigned long value)
+bool intutil_is_pow2(unsigned long value)
 {
     /// Test is the input one of the power series of 2.
     return !( value & ( value - 1 ) );
 }
 
 static inline
-bool is_pow2_ull(unsigned long long value)
+bool intutil_is_pow2_ull(unsigned long long value)
 {
     /// Test is the input one of the power series of 2.
     return !( value & ( value - 1 ) );
 }
 
 static inline
-unsigned long next_pow2(unsigned long value)
+unsigned long intutil_ceil_pow2(unsigned long value)
 {
-    /// Calculate the next value of power series of 2 that
-    /// be great equal then the specific input @a value.
+    /// Calculate the smallest value of power series of 2 that
+    /// is great equal than the specific input @a value.
     --value;
 
     value |= value >> 1;
@@ -57,9 +57,9 @@ unsigned long next_pow2(unsigned long value)
 }
 
 static inline
-unsigned long long next_pow2_ull(unsigned long long value)
+unsigned long long intutil_ceil_pow2_ull(unsigned long long value)
 {
-    /// Unsigned long long version of ::next_pow2.
+    /// Unsigned long long version of ::intutil_ceil_pow2.
     --value;
 
     value |= value >> 1;
@@ -80,24 +80,24 @@ unsigned long long next_pow2_ull(unsigned long long value)
 }
 
 static inline
-unsigned long next_mul_n(unsigned long value, unsigned long n)
+unsigned long intutil_ceil_mul_n(unsigned long value, unsigned long n)
 {
-    /// Calculate the next multiple value of @a n that
-    /// be great equal then the specific input @a value.
+    /// Calculate the smallest value of multiple of @a n that
+    /// is great equal than the specific input @a value.
     return ( value + ( n - 1 ) ) / n * n;
 }
 
 static inline
-unsigned long long next_mul_n_ull(unsigned long long value, unsigned long long n)
+unsigned long long intutil_ceil_mul_n_ull(unsigned long long value, unsigned long long n)
 {
-    /// Unsigned long long version of ::next_mul_n.
+    /// Unsigned long long version of ::intutil_ceil_mul_n.
     return ( value + ( n - 1 ) ) / n * n;
 }
 
 static inline
-unsigned long next_mul_pow2(unsigned long value, unsigned long n)
+unsigned long intutil_ceil_mul_pow2(unsigned long value, unsigned long n)
 {
-    /// Similarly to ::next_mul_n, but optimised for series of power 2 multiple.
+    /// Similarly to ::intutil_ceil_mul_n, but optimised for the multiple of series of power 2.
     /// @warning The input @a n must be one of the power series of 2,
     ///          or the behaviour bill be undefined.
     --n;
@@ -105,38 +105,38 @@ unsigned long next_mul_pow2(unsigned long value, unsigned long n)
 }
 
 static inline
-unsigned long long next_mul_pow2_ull(unsigned long long value, unsigned long long n)
+unsigned long long intutil_ceil_mul_pow2_ull(unsigned long long value, unsigned long long n)
 {
-    /// Unsigned long long version of ::next_mul_pow2.
+    /// Unsigned long long version of ::intutil_ceil_mul_pow2.
     --n;
     return ( value + n ) & ~n;
 }
 
 static inline
-unsigned long next_mul4(unsigned long value)
+unsigned long intutil_ceil_mul_4(unsigned long value)
 {
-    /// Similarly to ::next_mul_n, but optimised for multiple of 4.
+    /// Similarly to ::intutil_ceil_mul_n, but optimised for multiple of 4.
     return ( value + ( 4 - 1 ) ) & ~( 4 - 1 );
 }
 
 static inline
-unsigned long long next_mul4_ull(unsigned long long value)
+unsigned long long intutil_ceil_mul_4_ull(unsigned long long value)
 {
-    /// Unsigned long long version of ::next_mul4.
+    /// Unsigned long long version of ::intutil_ceil_mul_4.
     return ( value + ( 4 - 1 ) ) & ~( 4 - 1 );
 }
 
 static inline
-unsigned long next_mul8(unsigned long value)
+unsigned long intutil_ceil_mul_8(unsigned long value)
 {
-    /// Similarly to ::next_mul_n, but optimised for multiple of 8.
+    /// Similarly to ::intutil_ceil_mul_n, but optimised for multiple of 8.
     return ( value + ( 8 - 1 ) ) & ~( 8 - 1 );
 }
 
 static inline
-unsigned long long next_mul8_ull(unsigned long long value)
+unsigned long long intutil_ceil_mul_8_ull(unsigned long long value)
 {
-    /// Unsigned long long version of ::next_mul8.
+    /// Unsigned long long version of ::intutil_ceil_mul_8.
     return ( value + ( 8 - 1 ) ) & ~( 8 - 1 );
 }
 
